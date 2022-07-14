a91fa896e562710c87b835534c0cd3656f084bcd
Konrad <konradbieniek@gmail.com>
2022-07-14 23:20:16 +0200

    first react project

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,43 +1,63 @@
-'use strict';
+"use strict";
 
-console.log('App.js is running');
+console.log("App.js is running");
 
 //JSX - JavaScript XML
+
+var app = {
+	title: "Indecision App",
+	subtitle: "Put your life in the hands of a computer."
+};
+
 var template = React.createElement(
-    'div',
-    null,
-    React.createElement(
-        'h1',
-        null,
-        'Hello Bieniu'
-    ),
-    React.createElement(
-        'p',
-        null,
-        'Paragraph'
-    )
+	"div",
+	null,
+	React.createElement(
+		"h1",
+		null,
+		app.title
+	),
+	React.createElement(
+		"p",
+		null,
+		app.subtitle
+	)
 );
+
+var user = {
+	name: "Pan-Be",
+	age: 33,
+	location: "Bielsko-Biała"
+};
+
+function getLocation(location) {
+	if (location) {
+		return React.createElement(
+			"p",
+			null,
+			"Location: ",
+			location
+		);
+	}
+}
 
 var templateTwo = React.createElement(
-    'div',
-    null,
-    React.createElement(
-        'h1',
-        null,
-        'Konrad'
-    ),
-    React.createElement(
-        'p',
-        null,
-        'Age: 33'
-    ),
-    React.createElement(
-        'p',
-        null,
-        'Location: B-B'
-    )
+	"div",
+	null,
+	React.createElement(
+		"h1",
+		null,
+		user.name
+	),
+	React.createElement(
+		"p",
+		null,
+		"Age: ",
+		user.age
+	),
+	getLocation(user.location)
 );
 
-var appRoot = document.getElementById('app');
+var appRoot = document.getElementById("app");
 
 ReactDOM.render(templateTwo, appRoot);
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,21 +1,39 @@
-console.log('App.js is running');
+console.log("App.js is running")
 
 //JSX - JavaScript XML
+
+var app = {
+	title: "Indecision App",
+	subtitle: "Put your life in the hands of a computer.",
+}
+
 var template = (
-    <div>
-        <h1>Hello Bieniu</h1>
-        <p>Paragraph</p>
-    </div>
+	<div>
+		<h1>{app.title}</h1>
+		<p>{app.subtitle}</p>
+	</div>
 )
+
+var user = {
+	name: "Pan-Be",
+	age: 33,
+	location: "Bielsko-Biała",
+}
+
+function getLocation(location) {
+	if (location) {
+		return <p>Location: {location}</p>
+	}
+}
 
 var templateTwo = (
-    <div>
-        <h1>Konrad</h1>
-        <p>Age: 33</p>
-        <p>Location: B-B</p>
-    </div>
+	<div>
+		<h1>{user.name}</h1>
+		<p>Age: {user.age}</p>
+		{getLocation(user.location)}
+	</div>
 )
 
-var appRoot = document.getElementById('app')
+var appRoot = document.getElementById("app")
 
 ReactDOM.render(templateTwo, appRoot)
