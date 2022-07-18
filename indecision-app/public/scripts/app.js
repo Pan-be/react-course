5490d96825ade46c759e2f0c53135e0c007435f3
Konrad <konradbieniek@gmail.com>
2022-07-18 07:44:58 +0200

    arrow functions

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -6,7 +6,8 @@ console.log("App.js is running");
 
 var app = {
 	title: "Indecision App",
-	subtitle: "Put your life in the hands of a computer."
+	subtitle: "Put your life in the hands of a computer.",
+	options: ["One", "Two"]
 };
 
 var template = React.createElement(
@@ -17,29 +18,19 @@ var template = React.createElement(
 		null,
 		app.title
 	),
-	React.createElement(
+	app.subtitle && React.createElement(
 		"p",
 		null,
 		app.subtitle
+	),
+	React.createElement(
+		"p",
+		null,
+		app.options.length > 0 ? "Here are your options." : "No options"
 	)
 );
 
-var user = {
-	name: "Pan-Be",
-	age: 33,
-	location: "Bielsko-Biała"
-};
-
-function getLocation(location) {
-	if (location) {
-		return React.createElement(
-			"p",
-			null,
-			"Location: ",
-			location
-		);
-	}
-}
+var count = 0;
 
 var templateTwo = React.createElement(
 	"div",
@@ -47,15 +38,9 @@ var templateTwo = React.createElement(
 	React.createElement(
 		"h1",
 		null,
-		user.name
-	),
-	React.createElement(
-		"p",
-		null,
-		"Age: ",
-		user.age
-	),
-	getLocation(user.location)
+		"Count: ",
+		count
+	)
 );
 
 var appRoot = document.getElementById("app");
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -2,38 +2,28 @@ console.log("App.js is running")
 
 //JSX - JavaScript XML
 
-var app = {
+const app = {
 	title: "Indecision App",
 	subtitle: "Put your life in the hands of a computer.",
+	options: ["One", "Two"],
 }
 
-var template = (
+const template = (
 	<div>
 		<h1>{app.title}</h1>
-		<p>{app.subtitle}</p>
+		{app.subtitle && <p>{app.subtitle}</p>}
+		<p>{app.options.length > 0 ? "Here are your options." : "No options"}</p>
 	</div>
 )
 
-var user = {
-	name: "Pan-Be",
-	age: 33,
-	location: "Bielsko-Biała",
-}
+let count = 0
 
-function getLocation(location) {
-	if (location) {
-		return <p>Location: {location}</p>
-	}
-}
-
-var templateTwo = (
+const templateTwo = (
 	<div>
-		<h1>{user.name}</h1>
-		<p>Age: {user.age}</p>
-		{getLocation(user.location)}
+		<h1>Count: {count}</h1>
 	</div>
 )
 
-var appRoot = document.getElementById("app")
+const appRoot = document.getElementById("app")
 
 ReactDOM.render(templateTwo, appRoot)
