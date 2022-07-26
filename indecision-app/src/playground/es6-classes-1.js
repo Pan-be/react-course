8f93536007f5786178954efc7a596af10cdbfd66
Konrad Bieniek <konradbieniek@gmail.com>
2022-07-26 14:00:53 +0200

    completed lesson about classes

--- indecision-app/src/playground/es6-classes-1.js
+++ indecision-app/src/playground/es6-classes-1.js
@@ -3,13 +3,57 @@ class Person {
 		this.name = imie
 		this.age = age
 	}
-	getGreatings() {
+	getGreeting() {
 		return `Hi! My name is ${this.name} and I am ${this.age} year(s) old.`
 	}
 }
 
-const me = new Person("Konrad", 33)
-console.log(me.getGreatings())
+// class Student extends Person {
+// 	constructor(name, age, major) {
+// 		super(name, age)
+// 		this.major = major
+// 	}
+// 	hasMajor() {
+// 		return !!this.major
+// 	}
+// 	getGreeting() {
+// 		let description = super.getGreeting()
 
-const other = new Person()
-console.log(other.getGreatings())
+// 		if (this.hasMajor()) {
+// 			description += `My major is ${this.major}`
+// 		}
+
+// 		return description
+// 	}
+// }
+
+// const me = new Student("Stasiu", 33, 'Web dev')
+// console.log(me.hasMajor())
+// console.log(me.getGreeting())
+
+// const other = new Student()
+// console.log(other.hasMajor())
+// console.log(other.getGreeting())
+
+class Traveler extends Person {
+	constructor(name, age, homeLocation) {
+		super(name, age)
+		this.homeLocation = homeLocation
+	}
+	hasHomeLocation() {
+		return this.homeLocation
+	}
+	getGreeting() {
+		let greatings = super.getGreeting()
+		if (this.hasHomeLocation()) {
+			greatings += ` I'm visiting from ${this.homeLocation}.`
+		}
+		return greatings
+	}
+}
+
+const me = new Traveler('Konrad', 33, 'Bielsko-Biała')
+console.log(me.getGreeting());
+
+const other = new Traveler()
+console.log(other.getGreeting());
