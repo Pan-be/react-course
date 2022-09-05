4018216408a0d9a8ad3029c0f46db0a04a98d3d3
Konrad <konradbieniek@gmail.com>
2022-09-05 23:08:56 +0200

    default export

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,11 +1,18 @@
+// import validator from "validator"
+
 // import subtract, { square } from "./utils"
-import isSenior, { isAdult, canDrink } from "./person.js"
+// import isSenior, { isAdult, canDrink } from "./person.js"
 
-console.log(isAdult(10))
-console.log(canDrink(12))
+// console.log(isAdult(10))
+// console.log(canDrink(12))
 
-console.log(isSenior(70))
-console.log(isSenior(60))
+// console.log(isSenior(70))
+// console.log(isSenior(60))
 
 // console.log(square(10))
 // console.log(subtract(10, 5))
+
+// console.log(validator.isEmail("dupa@ggmail.com"))
+
+import React from "react"
+import ReactDOM from "react-dom"
